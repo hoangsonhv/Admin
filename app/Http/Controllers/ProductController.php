92ecbff5bca6cd63713b6f40d4e1d2769ef8470d
hoangsonhv <70937660+hoangsonhv@users.noreply.github.com>
2021-06-22 21:40:26 +0700

    son_22/06

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -2,9 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Products;
 use Illuminate\Http\Request;
 
 class ProductController extends Controller
 {
-    //
+    public function list(){
+//        $products = Products::all();
+        return view("product.product_list",[
+//            "products"=>$products
+        ]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
+use App\Http\Controllers\ProductController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -16,3 +17,5 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get("products",[ProductController::class,"list"]);
